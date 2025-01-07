2384e1350d84a7d130e3d105f36b6daca5aa5064
Gihoon <89381041+Qussong@users.noreply.github.com>
2025-01-07 12:20:18 +0900

    Step2 - Multi Thread
    사용자 요청이 들어올 때마다 Thread 를 새로 생성해서 사용자 요청을 처리하도록 한다.

--- was-practice-calculator/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice-calculator/src/main/java/org/example/CustomWebApplicationServer.java
@@ -19,53 +19,18 @@ public class CustomWebApplicationServer {
     }
 
     public void start() throws IOException {
-        // try 1 start
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             logger.info("[CustomWebApplicationServer] started {} port", port);
 
             Socket clientSocket;
             logger.info("[CustomWebApplicationServer] waiting for client.");
 
-            /**
-             * step1 - 사용자 요청을 메인 Thread가 처리
-             */
-
-            // while 1 start
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected.");
 
-                // try 2 start
-                try (InputStream in = clientSocket.getInputStream();
-                     OutputStream out = clientSocket.getOutputStream()) {
-
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    /*String line;
-                    while ((line = br.readLine()) != null) {
-                        System.out.println(line);
-                    }*/
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate2(operand1, operator, operand2);
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        // response setting
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }   // try 2 end
-            }   // while 1 end
-        }   // try 1 end
+                new Thread(new ClientRequestHandler(clientSocket)).start();
+            }
+        }
     }
 }
